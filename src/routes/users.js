ae12850365128f64442cdf30b7701463177fca7f
Adam Jedlička <mail@adamjedlicka.cz>
2022-04-24 13:07:34 +0200

    Přihlášení

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createUser } from '../db/users.js'
+import { createUser, getUser } from '../db/users.js'
 
 const router = express.Router()
 
@@ -8,14 +8,47 @@ router.get('/register', async (req, res) => {
 })
 
 router.post('/register', async (req, res) => {
+  try {
+    const name = req.body.name
+    const password = req.body.password
+
+    const user = await createUser(name, password)
+
+    res.cookie('token', user.token)
+
+    res.redirect('/')
+  } catch {
+    res.render('register', {
+      error: 'Registrace se nepovedla',
+    })
+  }
+})
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', async (req, res) => {
   const name = req.body.name
   const password = req.body.password
 
-  const user = await createUser(name, password)
+  const user = await getUser(name, password)
 
-  res.cookie('token', user.token)
+  if (user) {
+    res.cookie('token', user.token)
 
-  res.redirect('/')
+    res.redirect('/')
+  } else {
+    res.render('login', {
+      error: 'Chybné jméno nebo heslo',
+    })
+  }
+})
+
+router.get('/logout', (req, res) => {
+  res.cookie('token', undefined)
+
+  res.redirect('back')
 })
 
 export default router
