c63c2300886ca4b8ab9051ee08352c547c2584e7
Adam Jedlička <mail@adamjedlicka.cz>
2022-04-26 18:49:27 +0200

    Add dotenv

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 import { app } from './src/app.js'
 import { createWebSocketServer } from './src/websockets.js'
 
-const port = 3000
+const port = process.env.APP_PORT || 3000
 
 const server = app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
